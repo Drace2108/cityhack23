e8d1d391dc1554072f29553e91e710009c25b62e
Zhumakhan Arman <arman310501@gmail.com>
2023-02-11 20:17:29 +0800

    Arman's first update

--- src/components/MyCards/MyCardSettings.js
+++ src/components/MyCards/MyCardSettings.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import TextField from '@mui/material/TextField';
 import CircularProgress from '@mui/material/CircularProgress';
 import axios from "axios";
