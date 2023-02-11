2ceb21d084d7ccabd4ae68ef87e1f32e467417f8
Nurgazy Salanov <nurishok.s@gmail.com>
2023-02-11 17:41:40 +0800

    updated sidebars naming

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -24,7 +24,7 @@ export default function Sidebar() {
             className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-xl uppercase font-bold p-4 px-0"
             to="/"
           >
-            SFIDA
+            GreenFin
           </Link>
           {/* User */}
           <ul className="md:hidden items-center flex flex-wrap list-none">
