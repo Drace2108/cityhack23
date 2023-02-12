9c914a7f0de48be7962b54c3c06af8dd42efc6a0
Zhumakhan Arman <arman310501@gmail.com>
2023-02-12 11:30:35 +0800

    merge with Dale's branch and fix images

--- src/components/MyCards/MyInvestmentCardTable.js
+++ src/components/MyCards/MyInvestmentCardTable.js
@@ -140,7 +140,7 @@ export default function MyInvestmentCardTable({ color }) {
                   <div className="w-full md:w-4/12 px-4 mr-auto ml-auto">
                     <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-lightBlue-500">
                       <img
-                        src={require("assets/img/biomemakers.jpeg").default}
+                        src={require("assets/img/biomemakers.jpeg")}
                         className="w-full align-middle rounded-t-lg"
                         alt="Thumbnail"
                       />
