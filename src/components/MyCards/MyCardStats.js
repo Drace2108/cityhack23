3eb7eadf10fb5406c1034d7d7c8b03bd1f342abe
Drace2108 <dauka.iga@gmail.com>
2023-02-12 13:09:00 +0800

    investment update

--- src/components/MyCards/MyCardStats.js
+++ src/components/MyCards/MyCardStats.js
@@ -50,16 +50,6 @@ export default function MyCardStats({
   );
 }
 
-MyCardStats.defaultProps = {
-  statSubtitle: "Traffic",
-  statTitle: "350,897",
-  statArrow: "up",
-  statPercent: "3.48",
-  statPercentColor: "text-emerald-500",
-  statDescripiron: "Since last month",
-  statIconName: "far fa-chart-bar",
-  statIconColor: "bg-red-500",
-};
 
 MyCardStats.propTypes = {
   statSubtitle: PropTypes.string,
--- src/components/MyCards/MyInvestmentCard.js
+++ src/components/MyCards/MyInvestmentCard.js
@@ -57,7 +57,7 @@ export default function MyInvestmentCard({ color = "light" }) {
                 <div className="block w-full overflow-x-auto">
                     <section className="bg-blueGray-200 -mt-70">
                         <div className="flex flex-wrap">
-                            <div className="w-full lg:w-4/12 py-5" style={{paddingBottom:0}}>
+                            <div className="w-full lg:w-4/12 py-5" style={{ paddingBottom: 0 }}>
                                 <h1 className="investTextBlya">ACTIVE</h1>
                             </div>
                             <div className="w-full lg:w-4/12 px-4 py-5">
@@ -69,166 +69,165 @@ export default function MyInvestmentCard({ color = "light" }) {
                         </div>
 
                         <div className="flex flex-wrap">
-                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{paddingTop:0}}>
-                                
+                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{ paddingTop: 0 }}>
                                 <Link to="/admin/investment_page">
-                                <InvestmentStartups
-                                    statSubtitle="4 days left"
-                                    statTitle="Biome Makers"
-                                    statPercent="50"
-                                    statPercentColor="text-emerald-500"
-                                    statDescripiron="$600,000 / $1,200,000"
-                                    statIcon={biomemakers}
-                                />
+                                    <InvestmentStartups
+                                        statSubtitle="4 days left"
+                                        statTitle="Biome Makers"
+                                        statPercent="50"
+                                        statPercentColor="text-emerald-500"
+                                        statDescripiron="$600,000 / $1,200,000"
+                                        statIcon={biomemakers}
+                                    />
                                 </Link>
                             </div>
-                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{paddingTop:0}}>
-                                <InvestmentStartups
+                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{ paddingTop: 0 }}>
+                                <Link><InvestmentStartups
                                     statSubtitle="13 Feb"
                                     statTitle="Aurora"
                                     statPercent="79"
                                     statPercentColor="text-emerald-500"
                                     statDescripiron="$4,200,000 / $5,350,000"
                                     statIcon={aurora}
-                                />
+                                /></Link>
                             </div>
-                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{paddingTop:0}}>
-                                <InvestmentStartups
+                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{ paddingTop: 0 }}>
+                                <Link><InvestmentStartups
                                     statSubtitle="7 Feb"
                                     statTitle="Bottle Plus"
                                     statPercent="85"
                                     statPercentColor="text-emerald-500"
                                     statDescripiron="$1,700,000 / $2,000,000"
                                     statIcon={bottleplus}
-                                />
+                                /></Link>
                             </div>
                         </div>
                         <div className="flex flex-wrap">
-                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{paddingTop:0}}>
-                                <InvestmentStartups
+                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{ paddingTop: 0 }}>
+                                <Link><InvestmentStartups
                                     statSubtitle="17 days left"
                                     statTitle="Fuergy"
                                     statPercent="21"
                                     statPercentColor="text-emerald-500"
                                     statDescripiron="$680,000 / $3,200,000"
                                     statIcon={fuergy}
-                                />
+                                /></Link>
                             </div>
-                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{paddingTop:0}}>
-                                <InvestmentStartups
+                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{ paddingTop: 0 }}>
+                                <Link><InvestmentStartups
                                     statSubtitle="14 Feb"
                                     statTitle="EcoCart"
                                     statPercent="61"
                                     statPercentColor="text-emerald-500"
                                     statDescripiron="$880,000 / $1,450,000"
                                     statIcon={ecocart}
-                                />
+                                /></Link>
                             </div>
-                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{paddingTop:0}}>
-                                <InvestmentStartups
+                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{ paddingTop: 0 }}>
+                                <Link><InvestmentStartups
                                     statSubtitle="31 Jan"
                                     statTitle="Homeboy Electronics Recycling"
                                     statPercent="100"
                                     statPercentColor="text-emerald-500"
                                     statDescripiron="$1,800,000 / $1,800,000"
                                     statIcon={homeboy}
-                                />
+                                /></Link>
                             </div>
                         </div>
                         <div className="flex flex-wrap">
-                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{paddingTop:0}}>
-                                <InvestmentStartups
+                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{ paddingTop: 0 }}>
+                                <Link><InvestmentStartups
                                     statSubtitle="48 Days Left"
                                     statTitle="Viva Maris"
                                     statPercent="65"
                                     statPercentColor="text-emerald-500"
                                     statDescripiron="$585,000 / $900,000"
                                     statIcon={vivamaris}
-                                />
+                                /></Link>
                             </div>
-                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{paddingTop:0}}>
-                                <InvestmentStartups
+                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{ paddingTop: 0 }}>
+                                <Link><InvestmentStartups
                                     statSubtitle="18 Feb"
                                     statTitle="Heliogen"
                                     statPercent="96"
                                     statPercentColor="text-emerald-500"
                                     statDescripiron="$6,000,000 / $6,250,000"
                                     statIcon={heliogen}
-                                />
+                                /></Link>
                             </div>
-                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{paddingTop:0}}>
-                                <InvestmentStartups
+                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{ paddingTop: 0 }}>
+                                <Link><InvestmentStartups
                                     statSubtitle="26 Jan"
                                     statTitle="Encamp"
                                     statPercent="98"
                                     statPercentColor="text-emerald-500"
                                     statDescripiron="$6,440,000 / $6,570,000"
                                     statIcon={encamp}
-                                />
+                                /></Link>
                             </div>
                         </div>
                         <div className="flex flex-wrap">
-                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{paddingTop:0}}>
-                                <InvestmentStartups
+                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{ paddingTop: 0 }}>
+                                <Link><InvestmentStartups
                                     statSubtitle="51 days left"
                                     statTitle="Entomo Farms"
                                     statPercent="15"
                                     statPercentColor="text-emerald-500"
                                     statDescripiron="$195,000 / $1,300,000"
                                     statIcon={entomo}
-                                />
+                                /></Link>
                             </div>
-                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{paddingTop:0}}>
-                                <InvestmentStartups
+                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{ paddingTop: 0 }}>
+                                <Link><InvestmentStartups
                                     statSubtitle="23 feb"
                                     statTitle="Treeapp"
                                     statPercent="31"
                                     statPercentColor="text-emerald-500"
                                     statDescripiron="$1,080,000 / $3,590,000"
                                     statIcon={treeapp}
-                                />
+                                /></Link>
                             </div>
-                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{paddingTop:0}}>
-                                <InvestmentStartups
+                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{ paddingTop: 0 }}>
+                                <Link><InvestmentStartups
                                     statSubtitle="29 Dec"
                                     statTitle="Bluebird Climate"
                                     statPercent="97"
                                     statPercentColor="text-emerald-500"
                                     statDescripiron="$13,550,000 / $14,000,000"
                                     statIcon={bluebird}
-                                />
+                                /></Link>
                             </div>
                         </div>
                         <div className="flex flex-wrap">
-                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{paddingTop:0}}> 
-                                <InvestmentStartups
+                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{ paddingTop: 0 }}>
+                                <Link><InvestmentStartups
                                     statSubtitle="DATE: TBA"
                                     statTitle="Algenesis"
                                     statPercent="0"
                                     statPercentColor="text-emerald-500"
                                     statDescripiron="GOAL: $1,200,000"
                                     statIcon={algenesis}
-                                />
+                                /></Link>
                             </div>
-                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{paddingTop:0}}>
-                                <InvestmentStartups
+                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{ paddingTop: 0 }}>
+                                <Link><InvestmentStartups
                                     statSubtitle="5 March"
                                     statTitle="FuelGems"
                                     statPercent="4"
                                     statPercentColor="text-emerald-500"
                                     statDescripiron="$1,790,000 / $44,750,000"
                                     statIcon={fuelgems}
-                                />
+                                /></Link>
                             </div>
-                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{paddingTop:0}}>
-                                <InvestmentStartups
+                            <div className="w-full lg:w-4/12 px-4 py-5 lg:pt-8 pt-6 w-full md:w-4/12 px-4 text-center grow h-450-px" style={{ paddingTop: 0 }}>
+                                <Link><InvestmentStartups
                                     statSubtitle="18 Nov"
                                     statTitle="Handprint"
                                     statPercent="100"
                                     statPercentColor="text-emerald-500"
                                     statDescripiron="$3,340,000 / $3,340,000"
                                     statIcon={handprint}
-                                />
+                                /></Link>
                             </div>
                         </div>
                     </section>
